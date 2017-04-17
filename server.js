f2843d0c9946ee6b57eec8062596a1100fc6298f
DEMNATI <anas.demnati@capgemini.com>
2017-04-17 10:14:51 +0100

    switch to js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const Hapi = require('hapi');
 const Path = require('path');
 const Hoek = require('hoek');
+const annyang = require('annyang');
 
 const server = new Hapi.Server(~~process.env.PORT || 3000, '0.0.0.0');
 const defaultContext = {
@@ -33,6 +34,53 @@ server.route({
   handler: function (request, reply) {
     console.log('**request**');
     reply('Reply !');
+    if (annyang) {
+      // Let's define our first command. First the text we expect, and then the function it should call
+      var commands = {
+        'hide': function() {
+          console.log('hide');
+          // $('#talk_pool').val = 'hide';
+        },
+        'show': function() {
+          console.log('show');
+          // $('#talk_pool').value = 'show';
+        },
+        'delete': function() {
+          console.log('delete');
+          // $('#talk_pool').val('delete');
+        },
+        'add': function() {
+          console.log('add');
+          // $('#talk_pool').innerHTML = 'add';
+        }
+      };
+
+      // Add our commands to annyang
+      annyang.addCommands(commands);
+
+      annyang.addCallback('soundstart', function() {
+        console.log('sound detected');
+      });
+
+      annyang.addCallback('result', function() {
+        console.log('sound stopped');
+      });
+
+      annyang.addCallback('resultMatch', function(userSaid, commandText, phrases) {
+        console.log(userSaid); // sample output: 'hello'
+        console.log(commandText); // sample output: 'hello (there)'
+        console.log(phrases); // sample output: ['hello', 'halo', 'yellow', 'polo', 'hello kitty']
+        // $('#talk_pool').val = userSaid;
+        // $('#talk_pool').value = userSaid;
+        $('#talk_pool').val($('#talk_pool').val() + ' ' + userSaid);
+
+      });
+
+      // annyang.trigger('Hide');
+
+      // Start listening. You can call this here, or attach this call to an event, button, etc.
+      annyang.start();
+    }
   }
 });
 
